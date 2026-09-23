66fd768e47be01e9d57a56b8fbd906d5fd63ff8f
Carolina Faria Cassaro <cfariacassaro@gmail.com>
2026-09-23 11:39:22 -0300

    Add CHECK, UNIQUE and DEFAULT constraints; Refactor primary key declaration and creation order

--- banco_de_dados_aeroporto/creats.sql
+++ banco_de_dados_aeroporto/creats.sql
@@ -4,221 +4,159 @@ create database gerenciador_de_pista_aeroporto;
 
 use gerenciador_de_pista_aeroporto;
 
- create table Pista_de_Taxi(
-	id_pista_taxi int not null auto_increment,
-    designacao varchar(10),
-    sentido_de_traego varchar(20),
-    velocidade_maxima int,
-    primary key(id_pista_taxi)
- );
- 
- create table Pista(
-	id_pista int not null auto_increment,
-    designacao varchar(10),
-    peso_maximo_suportado decimal(8, 2),
-    iluminacao_de_aproximacao boolean,
-    primary key(id_pista)
- );
- 
- create table Area_de_Manobra(
-	id_area_manobra int not null auto_increment,
-    designacao varchar(50),
-    categoria_ICAO varchar(10),
-    elevacao decimal(6, 2),
-    primary key(id_area_manobra)
- );
- 
- alter table Pista_de_Taxi
- add column id_area_manobra int;
- 
- alter table Pista_de_Taxi
- add foreign key(id_area_manobra)
- references Area_de_Manobra(id_area_manobra);
- 
- alter table Pista
- add column id_area_manobra int;
- 
- alter table Pista
- add foreign key(id_area_manobra)
- references  Area_de_Manobra(id_area_manobra);
- 
- create table Hangar(
-	id_hangar int not null auto_increment,
-    capacidade int,
-    tipo varchar(30),
-    area decimal(8,2),
-    primary key(id_hangar)
- );
- 
  create table Infraestrutura(
-	id_infraestrutura int not null auto_increment,
-    localizacao varchar(100),
+	id_infraestrutura int primary key auto_increment,
+    localizacao varchar(100) UNIQUE,
     ocupado boolean,
     largura decimal(6,2),
     comprimento decimal(6,2),
-    primary key(id_infraestrutura)
  );
- 
- alter table Hangar
- add column id_infraestrutura int;
- 
- alter table Hangar
- add foreign key(id_infraestrutura)
- references Infraestrutura(id_infraestrutura);
- 
- alter table Area_de_Manobra
- add column id_infraestrutura int;
- 
- alter table Area_de_Manobra
- add foreign key(id_infraestrutura)
- references Infraestrutura(id_infraestrutura);
- 
- create table Portao(
-	id_portao int not null auto_increment,
-    id_infraestrutura int,
-    numero varchar(10),
+
+  create table Hangar(
+	id_hangar int primary key,
+    capacidade int,
+    tipo varchar(30),
+    area decimal(8,2),
+    foreign key(id_hangar) references Infraestrutura(id_infraestrutura),
+ );
+
+ create table Area_de_Manobra(
+	id_area_manobra int primary key,
+    designacao varchar(50),
+    categoria_ICAO varchar(10),
+    elevacao decimal(6, 2),
+    foreign key(id_area_manobra) references Infraestrutura(id_infraestrutura),
+ );
+
+  create table Portao(
+	id_portao int primary key,
+    numero varchar(10) UNIQUE,
     tipo_de_embarque varchar(30),
     terminal varchar(10),
-    primary key(id_portao),
-    foreign key(id_infraestrutura)
-    references Infraestrutura(id_infraestrutura)
+    foreign key(id_portao) references Infraestrutura(id_infraestrutura),
  );
+
+ create table Pista_de_Taxi(
+	id_pista_taxi int primary key,
+    designacao varchar(10),
+    sentido_de_traego varchar(20),
+    velocidade_maxima int,
+    foreign key(id_pista_taxi) references Area_de_Manobra(id_area_manobra),
+ );
+
  
+ create table Pista(
+	id_pista int primary key,
+    designacao varchar(10),
+    peso_maximo_suportado decimal(8, 2),
+    iluminacao_de_aproximacao boolean,
+    foreign key(id_pista) references Area_de_Manobra(id_area_manobra),
+ );
+
+ create table companhia(
+	id_companhia int primary key auto_increment,
+    nome varchar(100),
+    cnpj char(14) UNIQUE,
+    status varchar(20),
+    pais varchar(50),
+    dominio varchar(50) UNIQUE,
+);
+
+create table aeronave(
+	id_aeronave int primary key auto_increment,
+    id_companhia int,
+    codigo varchar(10) UNIQUE,
+    licenciada boolean,
+    status varchar(20),
+    foreign key(id_companhia)
+    references companhia(id_companhia)
+);
+
+ create table Operacao_de_Rota(
+	id_operacao_rota int primary key auto_increment,
+    id_aeronave int,
+    origem varchar(50),
+    destino varchar(50),
+    data_inicio datetime,
+    data_termino datetime,
+    objetivo varchar(45),
+    foreign key(id_aeronave) references aeronave(id_aeronave)
+);
+
  create table Operacao_de_Solo(
-	id_operacao_solo int not null auto_increment,
+	id_operacao_solo int primary key auto_increment,
+    id_operacao_rota int UNIQUE,
     id_infraestrutura int,
     proxima_operacao int,
     origem varchar(50),
     destino varchar(50),
     data_inicio datetime,
     data_termino datetime,
-    primary key(id_operacao_solo),
-    foreign key(id_infraestrutura)
-    references Infraestrutura(id_infraestrutura),
-    foreign key(proxima_operacao)
-    references Operacao_de_Solo(id_operacao_solo)
+    foreign key(id_operacao_rota) references Operacao_de_Rota(id_operacao_rota),
+    foreign key(id_infraestrutura) references Infraestrutura(id_infraestrutura),
+    foreign key(proxima_operacao) references Operacao_de_Solo(id_operacao_solo)
  );
  
-create table Operacao_de_Rota(
-	id_operacao_rota int not null auto_increment,
-    origem varchar(50),
-    destino varchar(50),
-    data_inicio datetime,
-    data_termino datetime,
-    objetivo varchar(45),
-    primary key(id_operacao_rota)
-);
-
-alter table Operacao_de_Solo
-add column id_operacao_rota int;
-
-alter table Operacao_de_Solo
-add foreign key(id_operacao_rota)
-references Operacao_de_Rota(id_operacao_rota);
-
-create table companhia(
-	id_companhia int not null auto_increment,
-    nome varchar(100),
-    cnpj char(14),
-    status varchar(20),
-    pais varchar(50),
-    dominio varchar(50),
-    primary key(id_companhia)
-);
-
-create table aeronave(
-	id_aeronave int not null auto_increment,
-    id_companhia int,
-    codigo varchar(10),
-    licenciada boolean,
-    status varchar(20),
-    primary key(id_aeronave),
-    foreign key(id_companhia)
-    references companhia(id_companhia)
-);
-
-alter table operacao_de_rota
-add column id_aeronave int;
-
-alter table operacao_de_rota
-add foreign key(id_aeronave)
-references aeronave(id_aeronave);
 
 create table usuario(
-	id_usuario int not null auto_increment,
+	id_usuario int primary key auto_increment,
     nome varchar(45),
-    cpf char(11),
-    e_mail varchar(45),
+    cpf char(11) UNIQUE,
+    e_mail varchar(45) UNIQUE,
     endereco varchar(150),
     senha varchar(255),
-    primary key(id_usuario)
 );
 
 create table setor(
-	id_setor int not null auto_increment,
+	id_setor int primary key auto_increment,
     nome varchar(50),
     funcao varchar(50),
-    primary key(id_setor)
 );
 
 create table funcionario(
-	id_usuario int not null,
+	id_funcionario int primary key,
     id_setor int,
-    matricula varchar(20),
+    matricula varchar(20) UNIQUE,
     cargo varchar(50),
     data_admissao date,
+    duracao_contrato int,
     status varchar(20),
-    primary key(id_usuario),
-    foreign key(id_usuario)
-    references usuario(id_usuario),
-    foreign key(id_setor)
-    references setor(id_setor)
+    foreign key(id_funcionario) references usuario(id_usuario),
+    foreign key(id_setor)references setor(id_setor)
 );
 
 create table operacoes_rota__funcionario(
-	id_operacao_rota int not null,
-    id_funcionario int not null,
+	id_operacao_rota int,
+    id_funcionario int,
     primary key(id_operacao_rota, id_funcionario),
-    foreign key(id_operacao_rota)
-    references operacao_de_rota(id_operacao_rota),
-    foreign key(id_funcionario)
-    references funcionario(id_usuario)
+    foreign key(id_operacao_rota) references operacao_de_rota(id_operacao_rota),
+    foreign key(id_funcionario)references funcionario(id_funcionario)
 );
 
 create table terceiro(
-	id_usuario int not null auto_increment,
+	id_terceiro int primary key,
     id_companhia int not null,
-    motivo_de_acesso varchar(100),
-    numero_de_identificacao varchar(20),
-    validade_de_acesso date,
-    primary key(id_usuario),
-    foreign key(id_usuario)
-    references usuario(id_usuario),
-    foreign key(id_companhia)
-    references companhia(id_companhia)
+    numero_de_identificacao varchar(20) UNIQUE,
+    foreign key(id_terceiro) references usuario(id_usuario),
+    foreign key(id_companhia) references companhia(id_companhia)
 );
 
 create table solicitacao(
-	id_solicitacao int not null auto_increment,
+	id_solicitacao int primary key auto_increment,
     id_funcionario int not null,
     id_aeronave int not null,
-    data date,
-    prazo date,
+    data_solicitacao date default current_date(),
+    prazo date check (prazo > data_solicitacao),
     conteudo text,
     status varchar(20),
-    primary key(id_solicitacao),
-    foreign key(id_funcionario)
-    references funcionario(id_usuario),
-    foreign key(id_aeronave)
-    references aeronave(id_aeronave)
+    foreign key(id_funcionario) references funcionario(id_funcionario),
+    foreign key(id_aeronave) references aeronave(id_aeronave)
 );
 
 create table terceiros_solicitacoes(
-	id_terceiro int not null,
-    id_solicitacao int not null,
+	id_terceiro int,
+    id_solicitacao int,
     primary key(id_terceiro, id_solicitacao),
-    foreign key(id_terceiro)
-    references terceiro(id_usuario),
-    foreign key(id_solicitacao)
-    references solicitacao(id_solicitacao)
+    foreign key(id_terceiro) references terceiro(id_usuario),
+    foreign key(id_solicitacao) references solicitacao(id_solicitacao)
 );
